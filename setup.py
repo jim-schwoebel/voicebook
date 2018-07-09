dd8fc7d5855b5bc6ea5a1ff2646c4fba9fa617bd
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2018-07-09 16:28:29 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+Install dependencies
+
+Let’s first start off this chapter installing the required dependencies. Assuming you’re on a mac, you can install all the libraries with these commands:
+
+cd ~
+	pip3 install pydub
+pip3 install librosa 
+pip3 install SpeechRecognition
+pip3 install wave
+pip3 install ffmpy 
+
+	git clone
+
+# below 
+
+Libraries
+
+pyAudioAnalysis 
+pydub
+Librosa
+Wave
+Ffmpy 
+Matplotlib 
+From gdetect import genderdetect as gd 
