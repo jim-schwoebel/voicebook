5912e4d40bb5d3a7f14009fef896a828c8b4b47e
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2018-07-25 16:58:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 # Install dependencies
 import os
 
+def install(modules):
+  for i in range(len(modules)):
+      os.system('pip3 install %s'%(modules[i]))
+
+modules=['scikit-learn','tpot','numpy','nltk', 'SpeechRecognition',
+        'spacy', 'librosa', 'TextBlob']
+install(modules)
+
+# things that need some custom setup 
 os.system('brew install opus') 
-os.system('brew install taglib')
+os.system('brew install portaudio')
+os.system('brew install sox')
 os.system('python3 -m spacy.en.download all')
 os.system("python3 -m spacy download 'en_core_web_sm'")
-os.system('pip3 install scikit-learn')
-os.system('pip3 install megaman') 
+# nltk add-ons 
