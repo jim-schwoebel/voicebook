01180350d15acfec1e4515dcccf9b68341f2573d
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2018-08-05 12:20:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,29 +10,32 @@ and assumes an iOS operating system.
 # Install dependencies
 import os
 
-def install(modules):
+def pip3_install(modules):
   for i in range(len(modules)):
-      os.system('pip3 install %s'%(modules[i]))
+    os.system('pip3 install %s'%(modules[i]))
 
+def brew_install(modules):
+  for i in range(len(modules)):
+      os.system('brew install %s'%(modules[i]))
+      
 # things that need some custom setup 
-os.system('brew install opus') 
-os.system('brew install portaudio')
-os.system('brew install sox')
-os.system('brew install nginx')
 os.system('pip3 install --upgrade setuptools')
 os.system('pip3 install -U pyobjc')
 
-modules=['scikit-learn','tpot','numpy','nltk', 'SpeechRecognition',
-        'spacy', 'librosa', 'TextBlob', 'matplotlib','bokeh',
-        'tensorflow','keras','textgenrnn', 'sumy', 'drawnow',
-        'matplotlib','seaborn', 'scipy', 'wordcloud', 'pybluez',
-        'wireless', 'pyserial', 'flask', 'django', 'uwsgi',
-        'virtualenv']
+brew_modules=['opus','portaudio','sox','nginx']
 
-install(modules)
+pip3_modules=['scikit-learn','tpot','numpy','nltk', 'SpeechRecognition',
+              'spacy', 'librosa', 'TextBlob', 'matplotlib','bokeh',
+              'tensorflow','keras','textgenrnn', 'sumy', 'drawnow',
+              'matplotlib','seaborn', 'scipy', 'wordcloud', 'pybluez',
+              'wireless', 'pyserial', 'flask', 'django', 'uwsgi',
+              'virtualenv']
 
+brew_install(brew_modules)
+pip3_install(pip3_modules)
+
+# customize spacy packages 
 os.system('python3 -m spacy.en.download all')
 os.system("python3 -m spacy download 'en_core_web_sm'")
 # download all nltk packages 
-import nltk
 nltk.download()
