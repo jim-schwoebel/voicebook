50de289bdc4e6b8c6b30ed40326219309b189b2a
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2018-09-28 17:21:49 -0400

    Update train_w2vclassify.py

--- chapter_4_modeling/train_w2vclassify.py
+++ chapter_4_modeling/train_w2vclassify.py
@@ -1,8 +1,61 @@
 '''
-train w2vclassify.py
+================================================ 
+##            VOICEBOOK REPOSITORY            ##      
+================================================ 
 
-Author: @Jim Schwoebel
-Title: Train Word2Vec
+repository name: voicebook 
+repository version: 1.0 
+repository link: https://github.com/jim-schwoebel/voicebook 
+author: Jim Schwoebel 
+author contact: js@neurolex.co 
+description: a book and repo to get you started programming voice applications in Python - 10 chapters and 200+ scripts. 
+license category: opensource 
+license: Apache 2.0 license 
+organization name: NeuroLex Laboratories, Inc. 
+location: Seattle, WA 
+website: https://neurolex.ai 
+release date: 2018-09-28 
+
+This code (voicebook) is hereby released under a Apache 2.0 license license. 
+
+For more information, check out the license terms below. 
+
+================================================ 
+##               LICENSE TERMS                ##      
+================================================ 
+
+Copyright 2018 NeuroLex Laboratories, Inc. 
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at 
+
+     http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License. 
+
+================================================ 
+##               SERVICE STATEMENT            ##        
+================================================ 
+
+If you are using the code written for a larger project, we are 
+happy to consult with you and help you with deployment. Our team 
+has >10 world experts in Kafka distributed architectures, microservices 
+built on top of Node.js / Python / Docker, and applying machine learning to 
+model speech and text data. 
+
+We have helped a wide variety of enterprises - small businesses, 
+researchers, enterprises, and/or independent developers. 
+
+If you would like to work with us let us know @ js@neurolex.co. 
+
+================================================ 
+##            TRAIN_W2VCLASSIFY.PY            ##    
+================================================ 
 
 This script takes in two lists of sentences, which are then broken down to their
 respective words. Word2vec embedding models are then created for each of these
@@ -20,8 +73,6 @@ For example,
 
 #This embedding also suits itself to add other similar dimensional feature vectors for images and/or audio as well
 #to increase model accuracy into the future. 
-
-(C) 2018, NeuroLex Laboratories 
 
 '''
 
