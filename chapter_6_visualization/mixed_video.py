1a010cd8543bfbd32c56e496205fea036c07475f
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2018-09-28 17:36:33 -0400

    Update mixed_video.py

--- chapter_6_visualization/mixed_video.py
+++ chapter_6_visualization/mixed_video.py
@@ -1,5 +1,61 @@
 '''
-Mixed_video.py
+================================================ 
+##            VOICEBOOK REPOSITORY            ##      
+================================================ 
+
+repository name: voicebook 
+repository version: 1.0 
+repository link: https://github.com/jim-schwoebel/voicebook 
+author: Jim Schwoebel 
+author contact: js@neurolex.co 
+description: a book and repo to get you started programming voice applications in Python - 10 chapters and 200+ scripts. 
+license category: opensource 
+license: Apache 2.0 license 
+organization name: NeuroLex Laboratories, Inc. 
+location: Seattle, WA 
+website: https://neurolex.ai 
+release date: 2018-09-28 
+
+This code (voicebook) is hereby released under a Apache 2.0 license license. 
+
+For more information, check out the license terms below. 
+
+================================================ 
+##               LICENSE TERMS                ##      
+================================================ 
+
+Copyright 2018 NeuroLex Laboratories, Inc. 
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at 
+
+     http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License. 
+
+================================================ 
+##               SERVICE STATEMENT            ##        
+================================================ 
+
+If you are using the code written for a larger project, we are 
+happy to consult with you and help you with deployment. Our team 
+has >10 world experts in Kafka distributed architectures, microservices 
+built on top of Node.js / Python / Docker, and applying machine learning to 
+model speech and text data. 
+
+We have helped a wide variety of enterprises - small businesses, 
+researchers, enterprises, and/or independent developers. 
+
+If you would like to work with us let us know @ js@neurolex.co. 
+
+================================================ 
+##                MIXED_VIDEO.PY              ##    
+================================================ 
 
 Make a video from a folder of images.
 
@@ -7,8 +63,6 @@ The idea here is to print out the waveform (as .png file)
 underneath the text file in series.
 
 This helps show time-series data!
-
-
 '''
 import cv2, os, shutil, librosa 
 import soundfile as sf
