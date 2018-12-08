bee020007b93ea99f13a0542204b484082a055d3
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2018-12-08 12:25:11 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -98,7 +98,7 @@ pip3_modules=['scikit-learn','tpot','numpy','nltk', 'SpeechRecognition',
               'matplotlib','seaborn', 'scipy', 'wordcloud', 'pybluez',
               'wireless', 'pyserial', 'flask', 'django', 'uwsgi',
               'virtualenv', 'minio','pymongo', 'auth0-python', 'google-cloud-storage',
-              'pycryptodome', 'flask-pymongo', 'pyaudio']
+              'pycryptodome', 'flask-pymongo', 'pyaudio', 'pocketsphinx']
 
 brew_install(brew_modules)
 pip3_install(pip3_modules)
